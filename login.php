822e2862630bf48300ee1ad38d763aa34e9e84f2
John Stamos <troyiskachor@gmail.com>
2015-11-26 06:55:49 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Login</title>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+
+  <body>
+    <section class="container">
+      <div class="login">
+        <h2>
+          Database login
+        </h2>
+        <p>
+          <input type="username" placeholder="username">
+        </p>
+        <p>
+          <input type="password" placeholder="password">
+        </p>
+      </div>
+    </section>
+
+  </body>
+</html>
