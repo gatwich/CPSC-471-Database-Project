25eeca0d96f9a91f3c4d29e7b0cb2fec415485ba
John Stamos <troyiskachor@gmail.com>
2015-11-26 06:55:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,30 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>NHL Database</title>
-		<style>
-			body{
-				background: url("http://pre00.deviantart.net/ebd2/th/pre/f/2008/290/e/2/nhl_wallpaper_by_sim25_design.jpg");
-				background-size: cover;
-				background-position: top;
-				text-align: center;
-			}
-			h1 {
-				color: white;
-				font-size: 60px;
-				text-shadow:	-1px -1px 0 #000,
-								1px -1px 0 #000,
-								-1px 1px 0 #000,
-								1px 1px 0 #000; 
-				margin: 50px 0 0 0;
-				font-family: "Arial Black";
-				font-weight: normal;
-			}
-			p {
-				color: white;
-			}
-			input {
-				border: 0;
-				padding: 10px;
-				font-size: 18px;
-			}
-			input[type="submit"]{
-				background: red;
-				color: white;
-			}
-			select {
-				
-			}
-			ul {
-				padding: 2px;
-				background: black;
-			}
-			li {
-				text-align: right;
-				color: white;
-				display: inline;
-				padding-right: 10px;
-			}
-			a {
-				margin: 0px;
-				color: white;
-			}
-			href {
-				margin: 0px;
-				color: white;
-			}
-			
-			
-		</style>
-		
-	</head>
-	<body>
-		<span>
-		<ul>
-			<li><a href="#">Log-in</a></li>
-			<li><a href="#">Sign-in</a></li>
-		</ul>
-		</span>
+
+  <head>
+    <title>NHL Database</title>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+
+  <body>
+    <span>
+    <ul>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="signup.php">Sign up</a></li>
+    </ul>
+    </span>
         <h1>THE NHL DATABASE</h1>
-		<p> Here at THE NHL DATABASE you can search for any coach, player, arena you wish.  Start now! </p>
-		<input type="search" placeholder="Search">
-		<input type="submit">
-		<!--
-		<div>
-		//	<select>
-		//		<option value="Players">Kane</option>
-		//		<option value="Sedin">Sedin</option>
-		//		<option value="Crosby">Crosby</option>
-		//		<option value="Ovy">Ovy</option>
-		//	</select>
-		</div>
-		-->
+    <p> Here at THE NHL DATABASE you can search for any coach, player, arena you wish.  Start now! </p>
+    <input type="search" placeholder="Search">
+    <input type="submit">    <!--
+    <div>
+    //  <select>
+    //    <option value="Players">Kane</option>
+    //    <option value="Sedin">Sedin</option>
+    //    <option value="Crosby">Crosby</option>
+    //    <option value="Ovy">Ovy</option>
+    //  </select>
+    </div>
+    -->
     </body>
 </html>
