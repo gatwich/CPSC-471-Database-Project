1d6ad39d900e8f37d53bcb0148acf2a8d74b3a94
John Stamos <troyiskachor@gmail.com>
2015-11-29 19:25:09 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,38 @@
+<!DOCTYPE html>
 <html>
+
   <head>
-    <title>Login</title>
+    <title>NHL Database</title>
 		<link rel="stylesheet" type="text/css" href="mystyle.css">
   </head>
 
   <body>
-    <section class="container">
-      <div class="login">
-        <h2>
-          Database login
-        </h2>
-        <p>
-          <input type="username" placeholder="username">
-        </p>
-        <p>
-          <input type="password" placeholder="password">
-        </p>
+    <div id="container">
+
+      <div class="header">
+        <ul>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="signup.php">Sign up</a></li>
+        </ul>
       </div>
-    </section>
+
+      <div class="navigation">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="favorites.php">Favorites</a></li>
+          <li><a href="recent.php">Recent</a></li>
+        </ul>
+      </div>
+
+    <div class="content">
+      <div class="login">
+        <p>Database login</p>
+        <div class="fields">
+          <input type="username" placeholder="username">
+          <input type="password" placeholder="password">
+        </div>
+      </div>
+  </div>
 
   </body>
 </html>
