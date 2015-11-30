ad8b1220d2fbd0bdc86636ced8911b55a90f9aa5
John Stamos <troyiskachor@gmail.com>
2015-11-29 19:26:40 -0700

    Create recent.php

---
+++ recent.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>NHL Database</title>
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+  </head>
+
+  <body>
+    <div id="container">
+
+      <div class="header">
+        <ul>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="signup.php">Sign up</a></li>
+        </ul>
+      </div>
+
+      <div class="navigation">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="favorites.php">Favorites</a></li>
+          <li><a href="recent.php">Recent</a></li>
+        </ul>
+      </div>
+  </div>
+
+  </body>
+</html>
