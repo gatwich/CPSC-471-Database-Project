ae574f21c5ad6f7bc641e6def40a17cbd734c551
ccokeke <chidi2u@gmail.com>
2015-12-02 03:12:09 -0700

    Arena, conference (bug), and division search.
    Conference logic is not correct yet.

---
+++ search.php
@@ -0,0 +1,101 @@
+<?php
+require 'index.php';
+
+if (isset($_POST['submit'])) { //user text is submitted
+    echo "submitted";
+    echo "<br>";
+    	// Checkbox which checkbox is selected
+
+	//if arena is selected
+	if(isset($_POST['arenaCheck'])){ 
+		echo "arenacheck";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){ 
+	  	 $name = $_POST['input']; 
+		 echo $name;
+		 echo "<br>";
+		 //connect  to the database
+		$db=mysql_connect  ("localhost", "root",  "admin") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-select  the database to use
+		$mydb=mysql_select_db("nhl");
+		//-query  the database table
+		$sql="SELECT  Name, Capacity, Location FROM arena WHERE Name='" . $name ."'";
+		//-run  the query against the mysql query function
+		$result=mysql_query($sql);
+		while($row=mysql_fetch_array($result)){
+			    $Name  =$row['Name'];
+			    $Capacity=$row['Capacity'];
+			    $Location=$row['Location'];
+		//-display  the result of the array
+		echo  "<ul>\n";
+		echo  "<li>" . "<a  href=\"$Name".".php\">"   .$Location ." ".$Name . " " . $Capacity .  "</a></li>\n";
+		echo  "</ul>";
+		}
+		//}
+	}
+
+    	// if conference is selected
+	if(isset($_POST['conferenceCheck'])){ 
+		echo "conferencecheck";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){ 
+	  	 $name = $_POST['input']; 
+		 echo $name;
+		 echo "<br>";
+		 //connect  to the database
+		$db=mysql_connect  ("localhost", "root",  "admin") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-select  the database to use
+		$mydb=mysql_select_db("nhl");
+		//-query  the database table
+		$conference="SELECT Name FROM team WHERE Division = SELECT Name FROM division WHERE Conference ='" . $name ."'";
+		//-run  the query against the mysql query function
+		$result=mysql_query($conference);
+		while($row=mysql_fetch_array($result)){
+			    $Name  =$row['Name'];
+		//-display  the result of the array
+		echo  "<ul>\n";
+		echo  "<li>" . "<a  href=\"$Name".".php\">"   .$Name .  "</a></li>\n";
+		echo  "</ul>";
+		
+		}
+		//}
+	}
+
+
+	//if division is selected
+	else if(isset($_POST['divisionCheck'])) {
+		echo "divisioncheck";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){ 
+	  	 $name = $_POST['input']; 
+		 echo $name;
+		 echo "<br>";
+		 //connect  to the database
+		$db=mysql_connect  ("localhost", "root",  "admin") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-select  the database to use
+		$mydb=mysql_select_db("nhl");
+		//-query  the database table
+		$division="SELECT Name FROM team WHERE Division = '" . $name ."'";
+		
+		//-run  the query against the mysql query function
+		$result=mysql_query($division);
+		while($row=mysql_fetch_array($result)){
+			    $Name  =$row['Name'];
+		//-display  the result of the array
+		echo  "<ul>\n";
+		echo  "<li>" . "<a  href=\"$Name".".php\">"   .$Name .  "</a></li>\n";
+		echo  "</ul>";
+		
+		}
+		//}
+	
+	
+	}
+
+else{
+ 
+	}
+
+	
+}
+?>
