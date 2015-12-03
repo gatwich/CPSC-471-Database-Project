62ca59b7b504b42600034fdfe42d4078142b81d3
ccokeke <chidi2u@gmail.com>
2015-12-03 00:30:05 -0700

    new commit with troy's code + jimas conditions

--- search.php
+++ search.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) { //user text is submitted
     echo "<br>";*/
     	// Checkbox which checkbox is selected
 
-	//if **********ARENA*********** is selected
+	//if arena is selected
 	if(isset($_POST['arenaCheck'])){
 		/*echo "arenacheck";
 		echo "<br>";*/
@@ -60,52 +60,10 @@ if (isset($_POST['submit'])) { //user text is submitted
 		 //connect  to the database
 		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
 		//-query  the database table
-		$conference="SELECT Name FROM team WHERE Division = SELECT Name FROM division WHERE Conference ='" . $name ."'";
+		$conference="SELECT Name, Location, Arena, `Games Played`, Wins, Losses, points FROM team WHERE Division IN (SELECT Name FROM division WHERE Conference ='".$name."'".")";
 		//-run  the query against the mysql query function
 		$result=mysqli_query($db, $conference);
-		while($row=mysqli_fetch_array($result)){
-			    $Name  =$row['Name'];
-		//-display  the result of the array
-		echo  "<ul>\n";
-		echo  "<li>" . "<a  href=\"$Name".".php\">"   .$Name .  "</a></li>\n";
-		echo  "</ul>";
-
-		}
-		//}
-	}
-
-
-	//if **************division************** is selected
-	else if(isset($_POST['divisionCheck'])) {
-	/*	echo "divisioncheck";
-		echo "<br>";*/
-		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){
-	  	 $name = $_POST['input'];
-/*		 echo $name;
-		 echo "<br>";
-		 echo "<br>";*/
-		 //connect  to the database
-		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
-		//-query  the database table
-		$division="SELECT Name, Location, Arena, 'Games Played', Wins, Losses, points FROM team WHERE Division = '" . $name ."'";
-
-		//-run  the query against the mysql query function
-		$result=mysqli_query($db, $division);
-  	echo "<div class=tableTitle>$name</div>";
-
-    echo "
-    <table cellspacing='0'>
-    <thead>
-      <th>Name</th>
-      <th>Location</th>
-      <th>Arena</th>
-      <th>Games Played</th>
-      <th>Wins</th>
-      <th>Losses</th>
-      <th>Points</th>
-    </thead>
-    </table>
-    ";
+		echo $name . " Conference";
 		while($row=mysqli_fetch_array($result)){
 			    $Name  =$row['Name'];
 			    $Location=$row['Location'];
@@ -115,34 +73,160 @@ if (isset($_POST['submit'])) { //user text is submitted
 			    $Losses=$row['Losses'];
 			    $points=$row['points'];
 		//-display  the result of the array
-    echo"<table cellspacing='0'>
+		echo  "<ul>\n";
+		echo "Name = " . $Name . " Location = " . $Location . " Arena = "   .$Arena . " Games Played = "   .$GP . " Wins = "   .$Wins . " Losses = "   .$Losses . " Points = "   .$points;
+		echo  "</ul>";
+
+		}
+	}
+
+
+	//if **************division************** is selected
+	else if(isset($_POST['divisionCheck'])) {
+		echo "divisioncheck";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){
+	  	 $name = $_POST['input'];
+		 echo $name;
+		 echo "<br>";
+		 echo "<br>";
+		 //connect  to the database
+		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-query  the database table
+		$division="SELECT Name, Location, Arena, `Games Played`, Wins, Losses, points FROM team WHERE Division = '" . $name ."'";
+
+		//-run  the query against the mysql query function
+		$result=mysqli_query($db, $division);
+  	
+		 echo "<table><tr><th>Name</th><th>Location</th><th>Arena</th><th>Games Played</th><th>Wins</th><th>Losses</th><th>Points</th></tr>";
+
+		while($row=mysqli_fetch_array($result)){
+		echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["Location"]."</td><td>" . $row["Arena"]. "</td><td>" . $row["Games Played"]. "</td><td>" . $row["Wins"]. "</td><td>" . $row["Losses"]. "</td><td>" . $row["points"]. "</td></tr>";
+		}
+		echo "</table>";
+  
+		//}
+
+	}
+
+	//if players is selected
+	else if(isset($_POST['playersCheck'])){ 
+		//echo "<strong>acheck</strong>";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){ 
+	 	$name = $_POST['input']; 
+		echo "<strong>$name</strong>";
+		echo "<br>";
+		echo "<br>";
+		//connect  to the database
+		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-query  the database table
+		$players="SELECT `First Name`, `Last Name`, Team, Goals, Points FROM players  WHERE `First Name` = '" . $name ."'";
+		//-run  the query against the mysql query function
+		$result=mysqli_query($db, $players);
+		while($row=mysqli_fetch_array($result)){
+			   $FirstName =$row['First Name'];
+			   $LastName=$row['Last Name'];
+			   $Team=$row['Team'];
+				$Goals=$row['Goals'];
+				$Points=$row['Points'];
+				//$Location=$row[''];
+		//-display  the result of the array
+		echo"<table cellspacing='0'>
+           <thead>
+             <th>First Name</th>
+             <th>Last Name</th>
+             <th>Team</th>
+	     <th>Goals</th>
+	     <th>Points</th>
+           </thead>
+
           <tbody>
             <tr>
-              <td>$Name</td>
-              <td>$Location</td>
-              <td>$Arena</td>
-              <td>$GP</td>
-              <td>$Wins</td>
-              <td>$Losses</td>
-              <td>$points</td>
+              <td>$FirstName</td>
+              <td>$LastName</td>
+              <td>$Team</td>
+	      <td>$Goals</td>
+	      <td>$Points</td>
+            </tr>
+          </tbody>
+        </table>
+        ";
+		
+		
+		
+		}
+		//}
+	}
+
+		//if Location is selected
+	else if(isset($_POST['locationCheck'])){
+		echo "<strong>locationcheck</strong>";
+		echo "<br>";
+		//if(preg_match("^/[A-Za-z]+/", $_POST['input'])){
+		  $name = $_POST['input'];
+		echo "<strong>$name</strong>";
+		echo "<br>";
+		echo "<br>";
+		//connect  to the database
+		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
+		//-query  the database table
+		$location="SELECT * FROM location WHERE City='" . $name ."'";
+		//-run  the query against the mysql query function
+		$result=mysqli_query($db, $location);
+		while($row=mysqli_fetch_array($result)){
+		   $City=$row['City'];
+		   $StateProvince=$row['State/Province'];
+		   $Country=$row['Country'];
+		//-display  the result of the array
+		echo"<table cellspacing='0'>
+           <thead>
+             <th>City</th>
+             <th>StateProvince</th>
+             <th>Country</th>
+           </thead>
+
+          <tbody>
+            <tr>
+              <td>$City</td>
+              <td>$StateProvince</td>
+              <td>$Country</td>
             </tr>
           </tbody>
         </table>
         ";
 		}
 		//}
-
-
 	}
 
-else{
+	// if the teams is selected
 
-	}
-  echo"
-  <input type='submit' name='submit' value='Favorite'>
-  <input type='submit' name='submit' value='Favorite'>
-  <input type='submit' name='submit' value='Favorite'>
-  ";
+	else if(isset($_POST['teamCheck'])){
+		echo "arenacheck";
+		echo "<br>";
+		$name = $_POST['input'];
+		echo $name;
+		//we connect to the database
+		$db=mysqli_connect  ("localhost", "root",  "admin", "nhl") or die ('I cannot connect to the database  because: ' . mysql_error());
+		$teams_query="SELECT  Name, Wins, Losses, points, `Games Played`, Location, Division FROM team WHERE `Name`='" . $name ."'";
+		$result=mysqli_query($db, $teams_query);
+		echo "<table><tr><th>Name</th><th>Wins</th><th>Losses</th><th>Points</th><th>Games Played</th><th>Location</th><th>Division</th></tr>";
 
+		while($row=mysqli_fetch_array($result)){
+		echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["Wins"]."</td><td>" . $row["Losses"]. "</td><td>" . $row["points"]. "</td><td>" . $row["Games Played"]. "</td><td>" . $row["Location"]. "</td><td>" . $row["Division"]. "</td></tr>";
+		}
+		echo "</table>";
+		}
+
+	else{
+
+		}
+
+	echo"
+	  <input type='submit' name='submit' value='Favorite'>
+	  <input type='submit' name='submit' value='Favorite'>
+	  <input type='submit' name='submit' value='Favorite'>
+	  ";
+ 
 }
 ?>
