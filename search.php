a7f807612352785ebf59ee50dcb19f99b96a6c37
ccokeke <chidi2u@gmail.com>
2015-12-03 01:19:35 -0700

    fixed conference, made styles the same

--- search.php
+++ search.php
@@ -21,31 +21,13 @@ if (isset($_POST['submit'])) { //user text is submitted
 		$arena="SELECT  Name, Capacity, Location FROM arena WHERE Name='" . $name ."'";
 		//-run  the query against the mysql query function
 		$result=mysqli_query($db, $arena);
+		echo "<strong>$arena. Arena</strong>";
+		echo "<table><tr><th>Name</th><th>Capacity</th><th>Location</th></tr>";
+
 		while($row=mysqli_fetch_array($result)){
-			    $Name  =$row['Name'];
-			    $Capacity=$row['Capacity'];
-			    $Location=$row['Location'];
-		//-display  the result of the array
-		echo"<table cellspacing='0'>
-           <thead>
-             <th>Name</th>
-             <th>Capacity</th>
-             <th>Location</th>
-           </thead>
-
-          <tbody>
-            <tr>
-              <td>$name</td>
-              <td>$Capacity</td>
-              <td>$Location</td>
-            </tr>
-          </tbody>
-        </table>
-        ";
-
-		/*echo  "<li>" . "<a  href=\"$Name".".php\">".$Name."</a></li>\n";*/
+		echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["Capacity"]."</td><td>" . $row["Location"]. "</td></tr>";
 		}
-		//}
+		echo "</table>";
 	}
 
     	// if *************conference*********** is selected
@@ -63,21 +45,13 @@ if (isset($_POST['submit'])) { //user text is submitted
 		$conference="SELECT Name, Location, Arena, `Games Played`, Wins, Losses, points FROM team WHERE Division IN (SELECT Name FROM division WHERE Conference ='".$name."'".")";
 		//-run  the query against the mysql query function
 		$result=mysqli_query($db, $conference);
-		echo $name . " Conference";
-		while($row=mysqli_fetch_array($result)){
-			    $Name  =$row['Name'];
-			    $Location=$row['Location'];
-			    $Arena=$row['Arena'];
-			    $GP=$row['Games Played'];
-			    $Wins=$row['Wins'];
-			    $Losses=$row['Losses'];
-			    $points=$row['points'];
-		//-display  the result of the array
-		echo  "<ul>\n";
-		echo "Name = " . $Name . " Location = " . $Location . " Arena = "   .$Arena . " Games Played = "   .$GP . " Wins = "   .$Wins . " Losses = "   .$Losses . " Points = "   .$points;
-		echo  "</ul>";
+		echo "<strong>$name. Conference</strong>";
+		echo "<table><tr><th>Name</th><th>Location</th><th>Arena</th><th>Games Played</th><th>Wins</th><th>Losses</th><th>Points</th></tr>";
 
+		while($row=mysqli_fetch_array($result)){
+		echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["Location"]."</td><td>" . $row["Arena"]. "</td><td>" . $row["Games Played"]. "</td><td>" . $row["Wins"]. "</td><td>" . $row["Losses"]. "</td><td>" . $row["points"]. "</td></tr>";
 		}
+		echo "</table>";
 	}
 
 
@@ -124,39 +98,13 @@ if (isset($_POST['submit'])) { //user text is submitted
 		$players="SELECT `First Name`, `Last Name`, Team, Goals, Points FROM players  WHERE `First Name` = '" . $name ."'";
 		//-run  the query against the mysql query function
 		$result=mysqli_query($db, $players);
-		while($row=mysqli_fetch_array($result)){
-			   $FirstName =$row['First Name'];
-			   $LastName=$row['Last Name'];
-			   $Team=$row['Team'];
-				$Goals=$row['Goals'];
-				$Points=$row['Points'];
-				//$Location=$row[''];
-		//-display  the result of the array
-		echo"<table cellspacing='0'>
-           <thead>
-             <th>First Name</th>
-             <th>Last Name</th>
-             <th>Team</th>
-	     <th>Goals</th>
-	     <th>Points</th>
-           </thead>
+		echo "<strong>$name.</strong>";
+		echo "<table><tr><th>First Name</th><th>Last Name</th><th>Team</th><th>Goals</th><th>Points</tr>";
 
-          <tbody>
-            <tr>
-              <td>$FirstName</td>
-              <td>$LastName</td>
-              <td>$Team</td>
-	      <td>$Goals</td>
-	      <td>$Points</td>
-            </tr>
-          </tbody>
-        </table>
-        ";
-		
-		
-		
+		while($row=mysqli_fetch_array($result)){
+		echo "<tr><td>" . $row["First Name"]. "</td><td>" . $row["Last Name"]."</td><td>" . $row["Team"]. "</td><td>" . $row["Goals"]. "</td><td>" . $row["Points"]. "</td>";
 		}
-		//}
+		echo "</table>";
 	}
 
 		//if Location is selected
@@ -174,29 +122,14 @@ if (isset($_POST['submit'])) { //user text is submitted
 		$location="SELECT * FROM location WHERE City='" . $name ."'";
 		//-run  the query against the mysql query function
 		$result=mysqli_query($db, $location);
-		while($row=mysqli_fetch_array($result)){
-		   $City=$row['City'];
-		   $StateProvince=$row['State/Province'];
-		   $Country=$row['Country'];
-		//-display  the result of the array
-		echo"<table cellspacing='0'>
-           <thead>
-             <th>City</th>
-             <th>StateProvince</th>
-             <th>Country</th>
-           </thead>
+		
+		echo "<strong>$name.</strong>";
+		echo "<table><tr><th>City</th><th>State/Province</th><th>Country</tr>";
 
-          <tbody>
-            <tr>
-              <td>$City</td>
-              <td>$StateProvince</td>
-              <td>$Country</td>
-            </tr>
-          </tbody>
-        </table>
-        ";
+		while($row=mysqli_fetch_array($result)){
+		echo "<tr><td>" . $row["City"]. "</td><td>" . $row["State/Province"]."</td><td>" . $row["Country"]. "</td>";
 		}
-		//}
+		echo "</table>";
 	}
 
 	// if the teams is selected
